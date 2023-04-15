aefd9a717f17ea509968f2b29489d5088894e67c
ElinaAndreeva <lolaclouds@yandex.ru>
2023-04-15 11:26:08 +0400

    Fixed null value mistake

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-        if (salary < 10000) {
+        if  (salary > 0) { if (salary < 10000) {
             double salary2 = salary * 0.85;
             System.out.println(salary2);
         } else if (salary > 10000 && salary <= 20000) {
@@ -11,7 +11,8 @@ public class SalaryCalculator {
         } else if (salary > 20000) {
             double salary2 = salary * 0.8;
             System.out.println(salary2);
-        } else if (salary < 0) {
+        } }
+        else if (salary < 0) {
             System.out.println("wrong input!");
         }
     }
